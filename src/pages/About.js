dc54ec8fddc37f052ee7ba5f8dfc2eafdb88396a
Ruphael Getahun <ruphaelg@Ruphaels-MacBook.local>
2022-06-11 21:43:27 -0400

    src

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
+import "./About.css";
+import profile_pic from '../assets/pic2.jpg';
+
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div class="split left">
+          <div className="centered">
+            <img
+              className="profile_image"
+              src={profile_pic}
+              alt="Profile Pic"
+            ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Ruphael Getahun</div>
+            <div className="brief_description">
+            <p> 
+          I am a rising Junior at Tufts University studying Economics and Computer Science. Some of my interests include playing basketball and trying new foods. A fun fact about me is that I have a twin sister.</p> 
+            </div>
+          </div>
+        </div>
       </div>
+      
+         
     )
   }
 }
